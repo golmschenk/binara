08013671320e2c7d0acdd3945f3bbabd8eb9f54d
golmschenk <greg@olmschenk.com>
2025-01-18 16:11:07 -0500

    Add temporary working directory context to enable other paths

--- tests/end_to_end_tests/test_basic_mcmc_run.py
+++ tests/end_to_end_tests/test_basic_mcmc_run.py
@@ -1,4 +1,10 @@
+from pathlib import Path
+
 import binara
 
+from tests.working_directory_context import use_working_directory
+
+
 def test_can_run_mcmc():
-    binara.run_mcmc(220052771, 6, 1, 0, 0, 0)
+    with use_working_directory(Path(__file__).parent):
+        binara.run_mcmc(220052771, 6, 1, 0, 0, 0)
