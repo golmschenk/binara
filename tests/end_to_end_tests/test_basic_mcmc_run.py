1186db23ba42bc45c163f0436a1fe76fe17cdfb7
golmschenk <greg@olmschenk.com>
2025-01-24 16:37:03 -0500

    Correct directory enforcing

--- tests/end_to_end_tests/test_basic_mcmc_run.py
+++ tests/end_to_end_tests/test_basic_mcmc_run.py
@@ -6,6 +6,6 @@ from tests.working_directory_context import use_working_directory
 
 
 def test_can_run_mcmc():
-    binara.enforce_expected_data_directory_tree.enforce_expected_data_directory_tree()
     with use_working_directory(Path(__file__).parent):
+        binara.enforce_expected_data_directory_tree.enforce_expected_data_directory_tree()
         binara.run_mcmc(220052771, 6, 1, 0, 0, 0)
