68f536730393bf666f82e06120ce136f923950fc
golmschenk <greg@olmschenk.com>
2025-01-24 13:51:47 -0500

    Add Github Actions debugging

--- tests/end_to_end_tests/test_basic_mcmc_run.py
+++ tests/end_to_end_tests/test_basic_mcmc_run.py
@@ -6,5 +6,9 @@ from tests.working_directory_context import use_working_directory
 
 
 def test_can_run_mcmc():
+    import faulthandler
+
+    faulthandler.enable(all_threads=True)
+
     with use_working_directory(Path(__file__).parent):
         binara.run_mcmc(220052771, 6, 1, 0, 0, 0)
