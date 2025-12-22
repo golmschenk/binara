f0d5819cf6f5d1ac3b30356897a6697c61bfa7fb
golmschenk <greg@olmschenk.com>
2025-12-22 15:53:03 -0500

    Rename the functions to internal

--- src/binara/__init__.py
+++ src/binara/__init__.py
@@ -1,2 +1,2 @@
-from .binara_ext import run_mcmc
+from .binara_ext import internal_run_mcmc
 from binara.internal.data_preparation import enforce_expected_data_directory_tree
--- src/binara_ext.cpp
+++ src/binara_ext.cpp
@@ -4,5 +4,7 @@
 
 NB_MODULE(binara_ext, module)
 {
-    module.def("run_mcmc", &Run_MCMC);
+    module.def("internal_run_mcmc", &Run_MCMC);
+    module.def("internal_calculate_light_curve", &Calculate_Lightcurve);
+    module.def("internal_calculate_log_likelihood", &Log_Likelihood);
 }
