cf6c6f0f6776461d366d2711997cbd878d2aa322
golmschenk <greg@olmschenk.com>
2026-01-05 19:50:14 -0500

    Add working light curve calculation from Python

--- src/binara_ext.cpp
+++ src/binara_ext.cpp
@@ -17,7 +17,6 @@ nb::ndarray<double, nb::numpy, nb::ndim<1>> internal_calculate_light_curve(
     auto* model_fluxes = new double[times_size];
     // Create the Python ownership object.
     nb::capsule owner(model_fluxes, [](void* p) noexcept { delete[] static_cast<double*>(p); });
-    std::cout << "Process: " << getprogname() << std::endl;
     Calculate_Lightcurve(times.data(), times_size, parameters.data(), model_fluxes);
     const size_t model_fluxes_shape[1] = {times_size};
     return {model_fluxes, 1, model_fluxes_shape, owner};
