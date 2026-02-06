3ee35cf7ac6ad9387a5a627bd9d0379f33d7166c
golmschenk <greg@olmschenk.com>
2026-02-06 13:56:07 -0500

    Correct paths

--- src/configuration.cpp
+++ src/configuration.cpp
@@ -76,12 +76,13 @@ std::filesystem::path Configuration::initialize_session_directory_path(
 {
     std::filesystem::path sessions_root_directory{"sessions"};
     std::filesystem::path session_directory = sessions_root_directory / std::format(
-        "sessions/tic_id_{}_sector_{}", tic_id, sector);
+        "tic_id_{}_sector_{}", tic_id, sector);
     if (std::filesystem::exists(session_directory))
     {
         std::cerr << "Output session directory `" << session_directory << "` already exists. Halting program.";
         exit(105);
     }
+    std::filesystem::create_directories(session_directory);
     return session_directory;
 }
 
@@ -107,8 +108,8 @@ std::tuple<std::filesystem::path, std::filesystem::path, std::filesystem::path, 
     {
         std::filesystem::copy("configuration.toml", session_directory_path / "configuration.toml");
     }
-    std::filesystem::path folded_observed_light_curve_path = session_directory_path / "folded_model_light_curve.txt";
-    std::filesystem::copy(data_directory_path / "folded_model_light_curve.txt", folded_observed_light_curve_path);
+    std::filesystem::path folded_observed_light_curve_path = session_directory_path / "folded_observed_light_curve.txt";
+    std::filesystem::copy(data_directory_path / "folded_observed_light_curve.txt", folded_observed_light_curve_path);
     std::filesystem::path magnitudes_and_colors_path = session_directory_path / "magnitudes_and_colors.txt";
     std::filesystem::copy(data_directory_path / "magnitudes_and_colors.txt", magnitudes_and_colors_path);
     std::filesystem::path py_initialize_path = session_directory_path / "py_initialize.txt";
@@ -116,7 +117,7 @@ std::tuple<std::filesystem::path, std::filesystem::path, std::filesystem::path, 
     std::filesystem::path states_path = session_directory_path / "states.txt";
     std::ofstream states_ofstream(states_path);
     std::filesystem::path folded_observed_and_and_model_light_curves_path = session_directory_path /
-        "folded_observed_and_and_model_light_curves.txt";
+        "folded_observed_and_model_light_curves.txt";
     std::ofstream folded_observed_and_and_model_light_curves_ofstream(folded_observed_and_and_model_light_curves_path);
     std::filesystem::path parameters_path = session_directory_path / "parameters.txt";
     std::ofstream parameters_ofstream(parameters_path);
--- tests/end_to_end_tests/basic_mcmc_run_new_data_structure/test_basic_mcmc_run.py
+++ tests/end_to_end_tests/basic_mcmc_run_new_data_structure/test_basic_mcmc_run.py
@@ -9,19 +9,26 @@ from tests.data_directory_manipulations import verify_directories_match
 from tests.working_directory_context import use_working_directory
 
 
-@pytest.mark.skip
 def test_can_run_mcmc():
     working_directory = Path(__file__).parent
     with use_working_directory(working_directory):
         data_directory = working_directory.joinpath('data')
         if data_directory.exists():
             shutil.rmtree(data_directory)
+        sessions_directory = working_directory.joinpath('sessions')
+        if sessions_directory.exists():
+            shutil.rmtree(sessions_directory)
         shutil.copytree(working_directory.joinpath('template_starting_data_directory'), data_directory)
-        binara.enforce_expected_data_directory_tree()
         binara.internal_run_mcmc(220052771, 6, 1, 0, 0, 0)
-        verify_directories_match(data_directory, working_directory.joinpath('expected_resulting_data_directory'))
+        verify_directories_match(sessions_directory,
+                                 working_directory.joinpath('expected_resulting_sessions_directory'))
         if data_directory.exists():
             try:
                 shutil.rmtree(data_directory)
+            except PermissionError:  # In the Windows tests, this can sporadically fail, but it's fine to skip it.
+                pass
+        if sessions_directory.exists():
+            try:
+                shutil.rmtree(sessions_directory)
             except PermissionError:  # In the Windows tests, this can sporadically fail, but it's fine to skip it.
                 pass
