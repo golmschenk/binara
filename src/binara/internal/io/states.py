6b889347c212f13923b6e4703c28a9867f7326f6
golmschenk <greg@olmschenk.com>
2026-04-14 13:50:59 -0400

    Add individual column extraction

--- src/binara/internal/io/states.py
+++ src/binara/internal/io/states.py
@@ -1,10 +1,12 @@
 import typing
+from io import StringIO
 from pathlib import Path
 
+import numpy.typing as npt
 import pandas as pd
 
 
-def load_states_data_frame_from_file(file_handle: Path | typing.TextIO) -> pd.DataFrame:
+def load_states_data_frame_from_states_file(file_handle: Path | StringIO) -> pd.DataFrame:
     """
     Loads the states output text file as a Pandas data frame.
 
@@ -16,3 +18,24 @@ def load_states_data_frame_from_file(file_handle: Path | typing.TextIO) -> pd.Da
         f'parameter{index}' for index in range(len(data_frame.columns) - 2)]
     data_frame.columns = column_names
     return data_frame
+
+def load_log_likelihoods_from_states_file(file_handle: Path | StringIO) -> npt.NDArray:
+    """
+    Loads the log likelihood from the output text file.
+
+    :param file_handle: The handle to the output file.
+    :return: The log likelihood array.
+    """
+    data_frame = load_states_data_frame_from_states_file(file_handle)
+    return data_frame['log_likelihood'].to_numpy()
+
+
+def load_iterations_from_states_file(file_handle: Path | StringIO) -> npt.NDArray:
+    """
+    Loads the log likelihood from the output text file.
+
+    :param file_handle: The handle to the output file.
+    :return: The log likelihood array.
+    """
+    data_frame = load_states_data_frame_from_states_file(file_handle)
+    return data_frame['iteration'].to_numpy()
